31f963b499fda9a0b7d0c2c1f40b04aeb241ab06
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-17 14:24:25 -0500

    implemented special attack

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
     }
   },
   computed: {
@@ -15,10 +16,14 @@ const app = Vue.createApp({
     },
     playerBarStyles() {
       return { width: `${this.playerHealth}%` }
+    },
+    canUseSpecialAttack() {
+      return this.currentRound % 3 !== 0
     }
   },
   methods: {
     attackMonster() {
+      this.currentRound ++
       // deal between 5 and 12 damage
       const damage = getRandomValue(5, 12)
       this.monsterHealth -= damage;
@@ -30,6 +35,14 @@ const app = Vue.createApp({
       const damage = getRandomValue(8, 15)
       this.playerHealth -= damage;
       if(this.playerHealth <= 0) this.playerHealth = 0
+    },
+    specialAttackMonster() {
+      this.currentRound ++
+      // deal between 10 and 25 damage
+      const damage = getRandomValue(10, 25)
+      this.monsterHealth -= damage;
+      if(this.monsterHealth <= 0) this.monsterHealth = 0
+      this.attackPlayer()
     }
   }
 })
